8bd24d9ea7194bf324770a868ecb3e73bad9a0c1
Дмитрий <1917-01_1917@mail.ru>
2022-02-03 23:25:36 +0400

    First commit

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService {
+    public double weight;
+    public double height;
+
+    public double calculate() {
+        double BMI = weight / (height * height);
+        return BMI;
+    }
+
+
+}
